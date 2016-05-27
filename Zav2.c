f3a6fd259d9f8b29a6f07886061b52721fdcc48a
Vlad667 <genjas213@gmail.com>
2016-05-27 09:13:24 +0300

    Create Zav2.c

---
+++ Zav2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+	double mas[300], n;
+	int i, size;
+	
+	    printf("Vvedite razmer massiva: ");
+	    scanf("%d", &size);
+	
+	printf("\nVvedite n = ");
+	scanf("%lf", &n);
+	
+	srand(time(0));
+	
+ for(i = 0; i < size; i++) {
+		mas[i] = ((float)rand() / RAND_MAX) * n;
+		printf("\n\tmas[%d] = %lf", i, mas[i]);
+
+	}
+	
+	system("pause");
+	return 0;
+}
