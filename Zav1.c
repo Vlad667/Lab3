2908ed7c70dc4e9eb6f010a2e9f21d8b2df36ef5
Vlad667 <genjas213@gmail.com>
2016-05-27 09:09:45 +0300

    Create Zav1.c

---
+++ Zav1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+    int get_random_int(int max_rand)
+{
+    return rand() % max_rand;   
+}
+    int get_random_int_min_max(int min_rand, int max_rand)
+{   
+    return min_rand + rand() % (max_rand - min_rand);
+}
+}
+
+           int main()
+{
+               int mas[100];
+               int n, i;
+                int max_rand;
+	
+                 printf("Vvedite n = ");
+                 scanf("%d", &n);
+
+
+
+                     for (i = 0; i < n; i++) {
+                     mas[i] = get_random_int_min_max(0, n);
+	                 printf("%d\n", mas[i]);
+    }
+    
+    system("pause");
+    return 0;
+}
