7a29543b014a44338b417fce1f3c369defef9d74
Vlad667 <genjas213@gmail.com>
2016-05-30 08:50:37 +0300

    Последовательность

---
+++ Zav7.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+  int main ()
+{
+	  int x;
+	  double mas[300];
+  	double a;
+
+	  for(x = 0, a = -10; a < 10; x++) {
+		a = a + 0.1;
+		mas[x] = a;
+		printf("%.1lf\n", a);
+
+	}	
+	system("pause");
+	return 0;
+} 
