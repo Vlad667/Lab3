dda529b1d048512015b83024d878f930cbce6a76
Vlad667 <genjas213@gmail.com>
2016-05-30 08:43:19 +0300

    Целые от 0 до y

---
+++ Zav5.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ()
+{
+	     int mas[300], x, y;
+	
+    	printf("Vvedite y = ");
+	   scanf("%d", &y);
+	
+	for(x = 1; x <= y; x++) {
+		mas[x] = x;
+		printf("\nmas[%d] = %d", x, mas[x]);
+	}	
+	system("pause");
+	return 0;
+}
