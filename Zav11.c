1791043d945b2424c0b702cb006b1e1a9b815d68
Vlad667 <genjas213@gmail.com>
2016-05-30 09:02:32 +0300

    Create Zav11.c

---
+++ Zav11.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int mas[100], i;
+	int size;
+	
+	printf("Razmer massiva = ");
+	scanf("%d", &size);
+	
+	for (i = 0; i < size; i++){
+		scanf("%d", &mas[i]);
+	}
+	
+	for (i = 0; i < size; i++){
+		printf ("\nmas[%d] = %d", i, mas[i]);
+	}
+	system("pause");
+	return 0;
+} 
